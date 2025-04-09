c65c3be651b2906830652d4de615159b06ef8083
tyoma <tvtyyiovsv@gmail.com>
2025-04-09 23:37:58 +0200

    complete Hero

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 function Header() {
     const Menu= [
@@ -20,9 +22,21 @@ function Header() {
         }
     ]
   return (
-    <div>
-        <Image src="./logo.svg" alt='logo' width={50} height={50}/>
+    <div className='flex items-center justify-between p-4 shadow-md'>
+        <div className='flex items-center gap-10'>
+            <Image src="./logo.svg" alt='logo' width={50} height={50}/>       
+            <ul className='md:flex gap-8 hidden'>
+                {Menu.map((item, index) => (
+                    <Link href={item.path} key={index}>
+                        <li className='hover:text-primary cursor-pointer hover:scale-110 transition-all ease-in-out'>{item.name}</li>
+                    </Link>
+                ))}
+            </ul>
+        </div>
+        <Button>Get Started</Button>
     </div>
+
+    
   )
 }
 
