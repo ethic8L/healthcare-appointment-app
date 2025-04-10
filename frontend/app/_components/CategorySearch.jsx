483d50cd41e1c6199b642ea33bfce15af8543168
tyoma <tvtyyiovsv@gmail.com>
2025-04-10 20:20:29 +0200

    combine frontend + backend

--- frontend/app/_components/CategorySearch.jsx
+++ frontend/app/_components/CategorySearch.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+import { Button } from '@/frontend/components/ui/button'
+import { Input } from '@/frontend/components/ui/input'
 import { Search } from 'lucide-react'
 import GlobalApi from '../_utils/GlobalApi'
 import Image from 'next/image'
