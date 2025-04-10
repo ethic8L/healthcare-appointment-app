a8844ac601a53ad92602062ed4a7bcaa5fc5cced
tyoma <tvtyyiovsv@gmail.com>
2025-04-10 20:04:40 +0200

    add category section

--- app/_components/CategorySearch.jsx
+++ app/_components/CategorySearch.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Search } from 'lucide-react'
+import GlobalApi from '../_utils/GlobalApi'
+import Image from 'next/image'
+
 
 function CategorySearch() {
+
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(()=>{
+    getCategoryList()
+  },[])
+
+  const getCategoryList=()=>{
+    GlobalApi.getCategory().then((res)=>{
+      console.log(res.data.data);
+      setCategoryList(res.data.data);
+    })
+  }
   return (
-    <div className='mb-10 items-center flex flex-col gap-2'>
+    <div className='mb-10 px-5 items-center flex flex-col gap-2'>
         <h2 className='font-bold text-4xl tracking-wide'>Search <span className='text-primary'>Doctors</span></h2>
         <h2 className='text-gray-400 text-xl'>Search Your Doctor and Book Appointment in one click</h2>    
 
@@ -13,6 +30,21 @@ function CategorySearch() {
             <Input type="text" placeholder="Search..." />
             <Button type="submit"><Search className='h-4 w-4 mr-2'/>Search</Button>
         </div>
+
+        {/* Display List of Category*/}
+        <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
+          {categoryList.map((item, index)=>index<6 && (
+            <div key={index} className='flex flex-col text-center items-center gap-2 p-5 m-2 rounded-lg bg-blue-50 hover:scale-110 transition-all ease-in-out mt-5 cursor-pointer'>
+              <Image  src={item.Icon?.[0]?.url} alt='icon' width={40} height={40}/>
+              <label className='text-blue-600 text-sm'>{item?.Name}</label>
+            </div>
+          ))}
+        </div>
+
+         
+
+        
+
     </div>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: 'localhost',
+            port: '1337',
+            pathname: '/uploads/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            pathname: '/**',
+          },
+        ],
+      },
+    experimental: {
+        allowedDevOrigins: ['http://192.168.1.32:3000',   'http://192.168.1.32:3000',
+            'http://localhost:3000',
+            'http://127.0.0.1:3000'], 
+    },
+};
 
 export default nextConfig;
